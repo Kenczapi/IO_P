c970e924131c1b9ba9b6d4653db3f2d6974a8f33
Kenczapi <kenczapi@o2.pl>
2019-05-30 22:44:13 +0200

    Dramat
    Zaczeta apka, generalnie stracilem chec do zycia przez tego xamarina

---
+++ Projekt_InzOpr/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InTheHand.Net;
+
+namespace Projekt_InzOpr
+{
+    public partial class Okno
+    { 
+        private BluetoothAddress MyBTMacAdress { get; set; }
+
+        public BluetoothAddress GetLocalMacAdress()
+        {
+            InTheHand.Net.Bluetooth.BluetoothRadio myAdress = InTheHand.Net.Bluetooth.BluetoothRadio.PrimaryRadio;
+
+            if (myAdress == null)
+            {
+                MessageBox.Show("Nie znaleziono adresu");
+
+                return null;
+            }
+
+            return myAdress.LocalAddress;
+        }
+
+
+    }
+}
